786eb7345479a7bbe949a24c2705bb912c46b1ba
jimatcx <84412582+jimatcx@users.noreply.github.com>
2024-02-07 15:36:48 -0600

    Update Attribution.pm

--- lib/DDG/App/Attribution.pm
+++ lib/DDG/App/Attribution.pm
@@ -29,6 +29,7 @@ sub BUILD {
 					print " - ".$key." (".$value.")\n";
      					print " VULN 1 ".$key." (".$value.")\n";
 	  				print " VULN 2 ".$key." (".$value.")\n";
+       					print " VULN 3 ".$key." (".$value.")\n";
        					print " VULN 4 ".$key." (".$value.")\n";
 				}
 			} else {
